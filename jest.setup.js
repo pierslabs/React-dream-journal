1683035b0b60c7e3d4870ea684de10627e864772
Pedro <pedrolosasp@gmail.com>
2023-01-19 15:44:13 +0100

    Config test mode vite.

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
+import getEnvironments from './src/helpers/getEnviroments';
 import 'whatwg-fetch';
 import 'setimmediate';
 require('dotenv').config({
-  path: '.env',
+  path: './test.env',
 });
+
+jest.mock('./src/helpers/getEnviroments', () => ({
+  getEnvironments: () => ({ ...process.env }),
+}));
